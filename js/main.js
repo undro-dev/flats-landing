94eb84f605f6ac0c859b7c22b384b28e4a83b7ff
Vitaly Undro <vitalyundro@gmail.com>
2023-07-30 21:57:26 +0300

    feat: add: yandex-map, mask for number, video-player

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-/* header__top-row--mobile*/
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.header__top-row');
@@ -9,4 +8,36 @@ navBtn.onclick = function () {
 	document.body.classList.toggle('no-scroll');
 };
 
-console.log(navBtn);
+mask('[data-tel-input]');
+
+// Удаляем '+' если больше ничего не введено, чтобы показать placeholder
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach(input => {
+	input.addEventListener('input', () => {
+		if (input.value == '+') input.value = '';
+	});
+	input.addEventListener('blur', () => {
+		if (input.value == '+') input.value = '';
+	});
+});
+
+ymaps.ready(init);
+function init() {
+	var myMap = new ymaps.Map('map', {
+		center: [59.943543, 30.338928],
+		zoom: 16,
+	});
+	(myGeoObject = new ymaps.GeoObject(
+		{
+			geometry: {
+				type: 'Point',
+				coordinates: [59.943543, 30.338928],
+			},
+		},
+		{
+			preset: 'islands#blackStretchyIcon',
+			draggable: false,
+		}
+	)),
+		myMap.geoObjects.add(myGeoObject);
+}
